9fced0c824bf09bcb67b168bdfe82c418fca6118
github_NalaboluChandanaKeerthi <github_n.chandanakeerthi@gmail.com>
2024-08-01 11:40:15 +0530

    second commit

--- evenodd.java
+++ evenodd.java
@@ -2,7 +2,7 @@ class evenodd{
   public static void main(String args[]){
   int num=5;
   if(num%2==0){
-  System.out.println("Even number");
+  System.out.println("number");
   }
   else{
   System.out.println("Odd number");
---
+++ fact1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class fact1{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number: ");
+int num=sc.nextInt();
+System.out.println("Enter a factorial: ");
+int fact=sc.nextInt();
+while(num>=1){
+fact=num*fact;
+num--;
+}
+System.out.println(fact);
+}
+}
